bb9ec7297625a7ff6237b4477c620833fbcc5f6c
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-20 19:40:36 -0600

    Added functionality to increase button

--- script.js
+++ script.js
@@ -5,5 +5,10 @@ const btn = document.querySelectorAll(".btn");
 btn.forEach(function (btn) {
   btn.addEventListener("click", (e) => {
     console.log(e.currentTarget.classList);
+    const styles = e.currentTarget.classList;
+    if (styles.contains("increase")) {
+      count++;
+    }
+    value.textContent = count;
   });
 });
